f950a40f7a19f96923658102322b0e41647f32c5
cheatermoves <lanners.marshall@yahoo.com>
2013-10-15 08:33:24 -0700

    oops I forgot one more route add

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ Portfolio::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
 
-   match 'posts/home' => 'home#index'
+
   match 'posts/:id/portfolio' => 'home#portfolio'
   match 'posts/:id/contact' => 'home#contact'
   match 'posts/:id/services' => 'home#services'
